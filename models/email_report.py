8fab27848df00bc943f98a3f2f71b4dc6f241759
Miguel <miguelhatrick@gmail.com>
2021-05-23 01:11:49 -0300

    added start and end date

--- models/email_report.py
+++ models/email_report.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import base64
 from datetime import datetime, timedelta, date, timezone
-from odoo import models, fields, api
 
+import pytz
+
+from odoo import models, fields, api
+from dateutil import relativedelta
 
 class EmailReport(models.Model):
     """Reports generated for email"""
@@ -12,6 +15,7 @@ class EmailReport(models.Model):
 
     months_choices = []
     years_choices = []
+    week_choices = []
     currentYear = datetime.now().year
 
     for i in range(1, 13):
@@ -20,15 +24,24 @@ class EmailReport(models.Model):
     for i in range(currentYear, currentYear + 5):
         years_choices.append((i, str(i)))
 
+    for i in range(1, 54):
+        week_choices.append((i, str(i)))
+
     name = fields.Char("Name", required=True)
 
     # TODO: Make the title shorter
     status_selection = [('pending', "Pending"), ('built', "Built")]
 
-    month = fields.Selection(months_choices, 'Month', required=True)
-    year = fields.Selection(years_choices, 'Year', required=True)
+    month = fields.Selection(months_choices, 'Month', required=False, default=None)
+    year = fields.Selection(years_choices, 'Year', required=False, default=None)
 
-    week_number = fields.Integer("Week number", required=False, default=None)
+    week_number = fields.Selection(week_choices, "Week number", required=False, default=None)
+
+    date_start = fields.Datetime("Report start", required=True, default=None)
+    date_end = fields.Datetime("Report end", required=True, default=None)
+
+    date_tz = fields.Selection('_tz_get', string='Timezone', required=True,
+                               default=lambda self: self.env.user.tz or 'UTC')
 
     state = fields.Selection(status_selection, 'Status', default='pending')
 
@@ -39,6 +52,67 @@ class EmailReport(models.Model):
     @api.multi
     def action_built(self):
         self.write({'state': 'built'})
+
+    @api.model
+    def _tz_get(self):
+        return [(x, x) for x in pytz.all_timezones]
+
+
+
+    @api.onchange('month', 'year')
+    def calculate_month_dates(self):
+        """Calculates the start and end date for the selected month"""
+
+        if not self.year or not self.month:
+            return
+
+        self.week_number = False
+
+        _tz = pytz.timezone(self.date_tz)
+
+        _date_start = _tz.localize(datetime.strptime(f'{self.year}-M{int(self.month)}-1', "%Y-M%m-%w"))
+        _date_end = _date_start + relativedelta.relativedelta(months=1) - timedelta(seconds=1)
+
+        # Convert to UTC before adding
+        self.date_start = (_date_start.astimezone(pytz.utc)).replace(tzinfo=None)
+        self.date_end = (_date_end.astimezone(pytz.utc)).replace(tzinfo=None)
+
+    @api.onchange('week_number', 'year')
+    def calculate_week_dates(self):
+        """Calculates the start and end date for the selected week"""
+
+        if not self.year or not self.week_number:
+            return
+
+        self.month = False
+
+        _tz = pytz.timezone(self.date_tz)
+
+        _date_start = _tz.localize(datetime.strptime(f'{self.year}-W{int(self.week_number) - 1}-1', "%Y-W%W-%w"))
+        _date_end = _date_start + timedelta(days=7) - timedelta(seconds=1)
+
+        # Convert to UTC before adding
+        self.date_start = (_date_start.astimezone(pytz.utc)).replace(tzinfo=None)
+        self.date_end = (_date_end.astimezone(pytz.utc)).replace(tzinfo=None)
+
+
+    def create_mass_mailing(self, _id):
+
+        html = self.env.ref('climbing_gym.email_report_web').render_qweb_html([_id])
+        # pdf result is a list
+        # html = base64.b64encode(pdf[0])
+
+        _send_date = datetime.now() + timedelta(minutes=5)
+
+
+        test = self.env['mail.mass_mailing'].create({
+            'name': 'TEST GUEROOOO',
+            'body_html': html[0].decode('utf-8'),
+            'state': 'in_queue',
+            'schedule_date': _send_date,
+            'contact_list_ids': [(6, 0, [self.env.ref('climbing_gym_report.mass_mail_list_weekly_report_climbing_gym').id])],
+        })
+        return test
 
     @api.multi
     def action_generate_attachment(self):
@@ -64,21 +138,3 @@ class EmailReport(models.Model):
                 'res_id': self.id,
                 'mimetype': 'application/x-pdf'
             })
-
-    def create_mass_mailing(self, _id):
-
-        html = self.env.ref('climbing_gym.email_report_web').render_qweb_html([_id])
-        # pdf result is a list
-        # html = base64.b64encode(pdf[0])
-
-        _send_date = datetime.now() + timedelta(minutes=5)
-
-
-        test = self.env['mail.mass_mailing'].create({
-            'name': 'TEST GUEROOOO',
-            'body_html': html[0].decode('utf-8'),
-            'state': 'in_queue',
-            'schedule_date': _send_date,
-            'contact_list_ids': [(6, 0, [self.env.ref('climbing_gym_report.mass_mail_list_weekly_report_climbing_gym').id])],
-        })
-        return test
